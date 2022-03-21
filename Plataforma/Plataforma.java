1e4a605490c0f7926ab8847361e17f5c920df84f
Leonardo Adler da Silva <88751032+LeoAdlerr@users.noreply.github.com>
2022-03-21 19:38:27 -0300

    Update Plataforma.java

--- Plataforma/Plataforma.java
+++ Plataforma/Plataforma.java
@@ -1,5 +1,35 @@
 package Plataforma;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Plataforma {
 
+     private List<Pessoa> pessoas = new LinkedList<Pessoa>();
+
+     public void cadastrarPessoa(Pessoa pessoa){
+          pessoas.add(pessoa);
+     }
+
+     public List<Pessoa> buscarPessoaPorRA(RA ra){
+           List<Pessoa> pessoasEncontradas = new LinkedList<Pessoa>();
+           for(Pessoa pessoa:pessoas){
+                if(pessoa.getRA().equals(RA)) pessoasEncontradas.add(pessoa);
+           }
+           return pessoasEncontradas;
+     }
+
+     private List<Disciplina> disciplinas = new LinkedList<Pessoa>();
+     
+     public void cadastrarDisciplina(Disciplina disciplina){
+          disciplinas.add(disciplina);
+     }
+     
+     public List<Pessoa> buscarDisciplinaPorNome(Nome nome){
+    	 List<Pessoa> disciplinasEncontradas = new LinkedList<Disciplina>();
+    	 for(Disciplina disciplina:disciplinas) {
+    		 if(disciplina.getNome().equals(Nome)) disciplinasEncontradas.add(disciplina);
+    	 }
+    	 return disciplinasEncontradas;
+          }
 }
